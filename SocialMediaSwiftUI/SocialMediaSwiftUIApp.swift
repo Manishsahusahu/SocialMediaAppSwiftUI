74f300ab3ec08edcdbc399c5495e45ae3e483c12
Manish Sahu <sahumanish327@gmail.com>
2025-04-08 07:54:28 +0530

    Initial Commit

---
+++ SocialMediaSwiftUI/SocialMediaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaSwiftUIApp.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocialMediaSwiftUITests/SocialMediaSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaSwiftUITests.swift
+//  SocialMediaSwiftUITests
+//
+//  Created by Manish sahu on 08/04/25.
+//
+
+import Testing
+@testable import SocialMediaSwiftUI
+
+struct SocialMediaSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
