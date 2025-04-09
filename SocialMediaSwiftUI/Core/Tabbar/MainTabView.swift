81b3ac52ae4a36ace2ddcb3a32cd9fd5102b7041
Manish Sahu <sahumanish327@gmail.com>
2025-04-09 08:04:47 +0530

    tab bar initial setup

---
+++ SocialMediaSwiftUI/Core/Tabbar/MainTabView.swift
@@ -0,0 +1,49 @@
+//
+//  MainTabView.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 09/04/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Tab {
+                Text("Home")
+            } label: {
+                Image(systemName: "house")
+            }
+            
+            Tab {
+                Text("Search")
+            } label: {
+                Image(systemName: "magnifyingglass")
+            }
+            
+            Tab {
+                Text("Post Upload")
+            } label: {
+                Image(systemName: "plus.square")
+            }
+            
+            Tab {
+                Text("Notifications")
+            } label: {
+                Image(systemName: "heart")
+            }
+            
+            Tab {
+                Text("Profile")
+            } label: {
+                Image(systemName: "person")
+            }
+        }
+        .tint(.primary)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
