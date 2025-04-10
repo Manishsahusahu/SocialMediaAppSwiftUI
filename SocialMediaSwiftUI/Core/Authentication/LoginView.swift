3c1aa3dc70a4f1176b09e013968dbc7f86633aca
Manish Sahu <sahumanish327@gmail.com>
2025-04-10 23:53:23 +0530

    textfields for email password

--- SocialMediaSwiftUI/Core/Authentication/LoginView.swift
+++ SocialMediaSwiftUI/Core/Authentication/LoginView.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
+        VStack {
+            HeaderView()
+            
+            InputFields()
+        }
+    }
+}
+
+private struct HeaderView: View {
+    var body: some View {
         Text(Strings.ManishSahu)
             .font(.largeTitle)
             .bold()
+    }
+}
+
+private struct InputFields: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter your Email", text: $email)
+                .modifier(TextFieldModifier()) 
+            
+            SecureField("Password", text: $password)
+                .modifier(TextFieldModifier())
+            
+            Button(action: { }) {
+                Text("Login")
+            }
+            .padding()
+        }
+    }
+}
+
+private struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .textInputAutocapitalization(.never)
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal, 24)
     }
 }
 
