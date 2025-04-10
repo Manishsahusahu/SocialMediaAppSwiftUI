a1aec86693853b2d73df93ca6c51075d995c6508
Manish Sahu <sahumanish327@gmail.com>
2025-04-10 23:08:15 +0530

    profile meta view and searchable modifier

--- SocialMediaSwiftUI/Core/Search/SearchView.swift
+++ SocialMediaSwiftUI/Core/Search/SearchView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
+    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0..<10, id: \.self) { _ in
-                    HStack {
-                        ProfileImageView(size: .small)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 16) {
+                    ForEach(0..<10, id: \.self) { _ in
+                        HStack {
+                            ProfileImageView(size: .small)
                             
+                            profileMetaView()
+                        }
                     }
                 }
+                .padding()
+                .searchable(text: $searchText, prompt: "Search...")
             }
+            .scrollIndicators(.hidden )
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
         }
+    }
+}
+
+private struct profileMetaView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Don Manish")
+                .fontWeight(.semibold)
+                
+            Text("Software Engineer")
+        }
+        .font(.footnote)
     }
 }
 
