52b9868ed8a9c264f81076b67c2be3d54b4210a9
Manish Sahu <sahumanish327@gmail.com>
2025-04-10 08:01:06 +0530

    profile view setup

---
+++ SocialMediaSwiftUI/Core/Profile/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 10/04/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        NavigationStack(path: Binding<NavigationPath>()) {
+            <#code#>
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
