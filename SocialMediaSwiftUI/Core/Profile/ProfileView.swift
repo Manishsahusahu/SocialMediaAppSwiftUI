935bd41500b32d9ef16ceed1c5ab2f4dfc722aeb
Manish Sahu <sahumanish327@gmail.com>
2025-04-10 20:38:10 +0530

    basic edit profile button and display image

--- SocialMediaSwiftUI/Core/Profile/ProfileView.swift
+++ SocialMediaSwiftUI/Core/Profile/ProfileView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        NavigationStack(path: Binding<NavigationPath>()) {
-            <#code#>
+        NavigationStack {
+            VStack {
+                HeaderView()
+                
+                EditProfileButton()
+            }
+        }
+    }
+}
+
+private struct HeaderView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Circle()
+                    .fill(Color.gray)
+                    .frame(width: 42, height: 42)
+                
+                Text("Don Manish")
+            }
+        }
+    }
+}
+
+private struct EditProfileButton: View {
+    var body: some View {
+        ZStack {
+            Text("Edit Profile")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundStyle(Color.primary)
         }
     }
 }
