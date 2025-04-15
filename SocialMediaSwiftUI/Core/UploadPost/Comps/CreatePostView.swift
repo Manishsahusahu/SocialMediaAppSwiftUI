98af0f7193d511f7bb650fc1205ab39fa7cc59e0
Manish Sahu <sahumanish327@gmail.com>
2025-04-15 15:47:42 +0530

    refactoring

---
+++ SocialMediaSwiftUI/Core/UploadPost/Comps/CreatePostView.swift
@@ -0,0 +1,37 @@
+//
+//  CreatePostView.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 15/04/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct CreatePostView: View {
+    @State private var caption: String = ""
+    @State private var isImagePickerPresented: Bool = false
+    @State private var photoItem: PhotosPickerItem? = nil
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            Rectangle()
+                .fill(Color(.systemGray))
+                .frame(width: 100, height: 100)
+            
+            TextField(
+                "Enter your caption...",
+                text: $caption,
+                axis: .vertical
+            )
+        }
+        .padding()
+        .onAppear {
+            isImagePickerPresented.toggle()
+        }
+        .photosPicker(
+            isPresented: $isImagePickerPresented,
+            selection: $photoItem
+        )
+    }
+}
