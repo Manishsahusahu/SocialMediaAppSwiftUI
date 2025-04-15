80adbb565f3783a59fead04853ceb7096b13a8ec
Manish Sahu <sahumanish327@gmail.com>
2025-04-15 15:39:15 +0530

    upload post header

---
+++ SocialMediaSwiftUI/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,22 @@
+//
+//  UploadPostView.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 15/04/25.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        VStack {
+            UploadPostToolbarView()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
