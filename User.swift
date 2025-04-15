244b8c7ea2771808a836c5d98059743a10b960ce
Manish Sahu <sahumanish327@gmail.com>
2025-04-15 14:07:18 +0530

    mock users

--- User.swift
+++ User.swift
@@ -15,3 +15,48 @@ struct User: Identifiable, Codable {
     var bio: String?
     let email: String
 }
+
+extension User {
+    static let MOCK_USERS: [User] = [
+        .init(
+            id: UUID().uuidString,
+            username: "manishsahu123",
+            profileImageUrl: "https://example.com/profile.jpg",
+            fullName: "Manish Sahu",
+            bio: "Hello, I am Manish Sahu",
+            email: "manishsahu@example.com"
+        ),
+        .init(
+            id: UUID().uuidString,
+            username: "batman",
+            profileImageUrl: "https://example.com/profile.jpg",
+            fullName: "Bruce Wayne",
+            bio: "Gotham's Dark knight",
+            email: "manishsahu@example.com"
+        ),
+        .init(
+            id: UUID().uuidString,
+            username: "venom",
+            profileImageUrl: "https://example.com/profile.jpg",
+            fullName: "Eddie Brock",
+            bio: "Venom",
+            email: "manishsahu@example.com"
+        ),
+        .init(
+            id: UUID().uuidString,
+            username: "ironman",
+            profileImageUrl: "https://example.com/profile.jpg",
+            fullName: "Tony Stark",
+            bio: "Billionaire",
+            email: "manishsahu@example.com"
+        ),
+        .init(
+            id: UUID().uuidString,
+            username: "spiderman",
+            profileImageUrl: "https://example.com/profile.jpg",
+            fullName: "Peter Parker",
+            bio: "Hanging creature",
+            email: "manishsahu@example.com"
+        ),
+    ]
+}
