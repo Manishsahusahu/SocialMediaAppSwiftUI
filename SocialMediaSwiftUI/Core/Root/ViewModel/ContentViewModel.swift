0f2a0129d683f7d10c2f63e403f76fae64529521
Manish Sahu <sahumanish327@gmail.com>
2025-04-15 18:27:26 +0530

    content viewmodel and read user session changes

---
+++ SocialMediaSwiftUI/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewModel.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 15/04/25.
+//
+
+import Foundation
+import FirebaseAuth
+import Combine
+
+class ContentViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    let authService = AuthService.shared
+    
+    init() {
+        setupPublisher()
+    }
+    
+    private func setupPublisher() {
+        authService.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
