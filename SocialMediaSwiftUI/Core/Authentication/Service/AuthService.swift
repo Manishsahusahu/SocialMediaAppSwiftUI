c5caec8f3c407ae05c13e6e0acd5c05ac6c84a60
Manish Sahu <sahumanish327@gmail.com>
2025-04-15 18:21:42 +0530

    currentuser

--- SocialMediaSwiftUI/Core/Authentication/Service/AuthService.swift
+++ SocialMediaSwiftUI/Core/Authentication/Service/AuthService.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class AuthService: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    private init() {
+        userSession = Auth.auth().currentUser
+    }
+    
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(email: String, password: String, username: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signOut() {
+        
+    }
+}
