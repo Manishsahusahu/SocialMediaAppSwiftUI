96946802eb3bc0d698b3ec56fc1b3738299dc326
Manish Sahu <sahumanish327@gmail.com>
2025-04-24 19:16:02 +0530

    upload image function

---
+++ SocialMediaSwiftUI/Services/ImageUploader.swift
@@ -0,0 +1,26 @@
+//
+//  ImageUploader.swift
+//  SocialMediaSwiftUI
+//
+//  Created by Manish sahu on 24/04/25.
+//
+
+import Firebase
+import FirebaseStorage
+import SwiftUI
+
+class ImageUploader {
+    static func UploadImage(image: UIImage) async throws -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 0.5) else { return nil }
+        let fileName = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_image/\(fileName)")
+        
+        do {
+            let _ = try await ref.putDataAsync(imageData)
+            return try await ref.downloadURL().absoluteString
+        } catch {
+            print("Failed to upload image: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
