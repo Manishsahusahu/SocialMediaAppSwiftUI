f19bdd968c9e955526aeaed63aa81bd240b68c42
Manish Sahu <sahumanish327@gmail.com>
2025-04-28 22:29:56 +0530

    height adjustment for logo

--- SocialMediaSwiftUI/Core/Root/View/ContentView.swift
+++ SocialMediaSwiftUI/Core/Root/View/ContentView.swift
@@ -15,6 +15,11 @@ struct ContentView: View {
     var body: some View {
         if viewModel.isLoadingUserData {
             Image(ImageName.logo)
+                .resizable()
+                .scaledToFit()
+                .containerRelativeFrame(.horizontal) { length, _ in
+                    length * 0.7
+                }
         } else {
             if let user = AuthService.shared.currentUser {
                 MainTabView(user: user)
